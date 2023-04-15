a9089127689e81762b17ff9c161a60e2646f02e2
Xanthron Writer <xanthronwriter@gmail.com>
2023-04-15 18:48:27 +0200

    Updated tests.

--- run_tests.sh
+++ run_tests.sh
@@ -1,11 +1,13 @@
 echo Wrap Images
 ./target/debug/presentation -v -f -o tests/presentations/wrap_images.rmd tests/head_page.rmd tests/notebooks/wrap_images.ipynb
-echo Python code block
-./target/debug/presentation -v -f -o tests/presentations/python.rmd tests/head_page.rmd tests/notebooks/python.ipynb
+
 echo Folders and subfolders
 ./target/debug/presentation -v -f -o tests/presentations/multiple_books.rmd tests/head_page.rmd tests/notebooks/multiple_books
+
 echo classes
 ./target/debug/presentation -v -f -o tests/presentations/class.rmd tests/head_page.rmd tests/notebooks/class.ipynb
 
+
+
 echo All pages
-./target/debug/presentation -v -f -o tests/presentations/all.rmd tests/head_page.rmd tests/notebooks/wrap_images.ipynb tests/notebooks/python.ipynb tests/notebooks/multiple_books
+./target/debug/presentation -v -f -o tests/presentations/all.rmd tests/head_page.rmd tests/notebooks/wrap_images.ipynb tests/notebooks/multiple_books tests/notebooks/class.ipynb
